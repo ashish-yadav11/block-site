b70f664ed769d3aaf2bc3f9c9f8306e22d0a0526
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2022-07-17 19:29:56 +0530

    Cleanup

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # Block Site
 
-**Block Site** (formerly known as Stop Social Media) is a simple Chrome extension that improves your productivity by blocking access to distracting websites as you specify.
+**Block Site** is a simple Chrome extension that improves your productivity by
+blocking access to distracting websites as you specify.
 
-### Usage
+# Usage
 
-To configure, click on the **B** icon in the toolbar and select **Options** from the menu. Enter sites in the textbox, leaving a new line for each site, like this:
+To configure, click on the **B** icon in the toolbar and select **Options**
+from the menu. Enter sites in the textbox, leaving a new line for each site
+like this:
 
 ```
 facebook.com
@@ -16,12 +19,4 @@ reddit.com
 !reddit.com/r/MachineLearning
 ```
 
-Sites starting with `!` will be exception to the rule, allowed.
-
-Choose what should be done when you try to visit a blocked site, options are:
-<ol type="A">
-<li>Close Tab</li>
-<li>Show Blocked info page (page that shows an information about the blocked site)</li>
-</ol>
-
-Last but not least, see a checkbox to enable/disable the blocking.
+Sites starting with `!` will be exceptions to the rule.
--- options.js
+++ options.js
@@ -7,51 +7,51 @@ const resolutionSelect = document.getElementById("resolution-select");
 const enabledToggle = document.getElementById("enabled-toggle");
 
 blockedList.placeholder = [
-  "facebook.com",
-  "instagram.com",
-  "youtube.com",
-  "!music.youtube.com",
-  "twitter.com",
-  "reddit.com",
-  "!reddit.com/r/MachineLearning",
+    "facebook.com",
+    "instagram.com",
+    "youtube.com",
+    "!music.youtube.com",
+    "twitter.com",
+    "reddit.com",
+    "!reddit.com/r/MachineLearning",
 ].join("\n");
 
 blockedList.addEventListener("change", (event) => {
-  const blocked = event.target.value.split("\n").map(s => s.trim()).filter(Boolean);
+    const blocked = event.target.value.split("\n").map(s => s.trim()).filter(Boolean);
 
-  chrome.storage.local.set({ blocked });
+    chrome.storage.local.set({ blocked });
 });
 
 resolutionSelect.addEventListener("change", (event) => {
-  const resolution = event.target.value;
+    const resolution = event.target.value;
 
-  chrome.storage.local.set({ resolution });
+    chrome.storage.local.set({ resolution });
 });
 
 enabledToggle.addEventListener("change", (event) => {
-  const enabled = event.target.checked;
+    const enabled = event.target.checked;
 
-  chrome.storage.local.set({ enabled });
+    chrome.storage.local.set({ enabled });
 });
 
 window.addEventListener("DOMContentLoaded", () => {
-  chrome.storage.local.get(["enabled", "blocked", "resolution"], function (local) {
-    const { enabled, blocked, resolution } = local;
-    if (!Array.isArray(blocked)) {
-      return;
-    }
+    chrome.storage.local.get(["enabled", "blocked", "resolution"], function (local) {
+        const { enabled, blocked, resolution } = local;
+        if (!Array.isArray(blocked)) {
+            return;
+        }
 
-    // blocked
-    var value = blocked.join("\r\n"); // display every blocked in new line
-    blockedList.value = value;
+        // blocked
+        var value = blocked.join("\r\n"); // display every blocked in new line
+        blockedList.value = value;
 
-    // resolution
-    resolutionSelect.value = resolution;
+        // resolution
+        resolutionSelect.value = resolution;
 
-    // enabled
-    enabledToggle.checked = enabled;
+        // enabled
+        enabledToggle.checked = enabled;
 
-    // UI ready
-    document.body.classList.add("ready");
-  });
+        // UI ready
+        document.body.classList.add("ready");
+    });
 });
